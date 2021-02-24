1fe32f52dbce6c79944b62544d8d52abeb0452b9
Dregu <dregu@dreg.us>
2021-02-24 22:01:57 +0200

    some fixes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # s2tracker
 Tracker for Spelunky 2 RTA speedrun categories.
+
+## Usage
+Start s2tracker, enable the tracker option in the autosplitter and go to http://localhost:27122/
--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const expressWs = require('express-ws')(app)
 const nconf = require('nconf')
 
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static('static'))
+app.use('/', express.static(__dirname + '/static'))
 
-var state = {}
+var state = { 'journal': Array(0xd1).fill(0).join(',') }
 
 const broadcast = (status) => {
   for(let client of expressWs.getWss().clients) {
@@ -21,17 +21,13 @@ app.post('/', (req, res) => {
     acc[key] = isNaN(+value) ? value : +value
     return acc
   },{})
-  console.log(state)
+  console.log('Got journal data from livesplit')
   broadcast(state)
-})
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/static/index.html')
 })
 
 app.ws('/', (ws, req) => {
   ws.on('message', (msg) => {
-    console.log(msg)
+    console.log('Client is asking for', msg)
     if(msg == 'state') {
       ws.send(JSON.stringify({ state: state }))
     }
@@ -39,4 +35,5 @@ app.ws('/', (ws, req) => {
 
 })
 
+console.log('Tracker is up on http://localhost:27122/')
 app.listen(27122)
